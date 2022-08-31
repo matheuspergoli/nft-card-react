a410052febd7995a85ca68fc90f077b5f230fa4e
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-08-30 21:27:38 -0300

    Update de animação

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,27 @@ const CardContainer = styled.main`
   width: 280px;
   padding: 15px;
   border-radius: 10px;
+  animation-name: showCard;
+  animation-duration: 350ms;
+  animation-fill-mode: forwards;
+  animation-timing-function: ease-out;
   background-color: ${({theme: { colors }}: TypeTheme) => colors.cardbg};
+
+  @keyframes showCard {
+   0% {
+    opacity: 0;
+    transform: translateX(-50px);
+   }
+
+   80% {
+    transform: translateX(20px);
+   }
+
+   100% {
+    opacity: 1;
+    transform: translateX(0);
+   }
+  }
 `
 
 function Card() {
